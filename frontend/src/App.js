6721529a4baeea505bcaee36b83f23b85cc6d35b
Sourav Lahon <lahoncs@gmail.com>
2023-07-22 18:59:52 +0530

    inspect disabled

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -50,7 +50,7 @@ function App() {
     store.dispatch(loadUser());
   });
 
-  // window.addEventListener("contextmenu", (e) => e.preventDefault());
+  window.addEventListener("contextmenu", (e) => e.preventDefault());
 
   return (
     <Router>
