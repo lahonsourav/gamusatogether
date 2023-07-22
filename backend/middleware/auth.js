072e2f3822c0cf5344091cc9317f442156363316
Sourav Lahon <lahoncs@gmail.com>
2023-07-22 18:59:00 +0530

    error message changed

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,7 +7,7 @@ exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
   const { token } = req.cookies;
 
   if (!token) {
-    return next(new ErrorHandler("Please Login to access this resource", 401));
+    return next(new ErrorHandler("Please Login", 401));
   }
 
   const decodedData = jwt.verify(token, process.env.JWT_SECRET);
@@ -22,7 +22,7 @@ exports.authorizeRoles = (...roles) => {
     if (!roles.includes(req.user.role)) {
       return next(
         new ErrorHandler(
-          `Role: ${req.user.role} is not allowed to access this resouce `,
+          `Role: ${req.user.role} can't access this, contact admin `,
           403
         )
       );
