08c9a58ba51508da4082a71705bd9dd1a2cd97e5
Sourav Lahon <lahoncs@gmail.com>
2023-07-22 17:48:33 +0530

    commented

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,12 +1,12 @@
 const sendToken = (user, statusCode, res) => {
   const token = user.getJWTToken();
 
-  const options = {
-    expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-  };
+  // const options = {
+  //   expires: new Date(
+  //     Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+  //   ),
+  //   httpOnly: true,
+  // };
 
   const expiresIn = process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000;
 
